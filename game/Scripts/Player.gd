b785f8303f990a1c22025874070ed8178301cdbc
Paweł Pilarski <pilarlysy@gmail.com>
2026-01-19 22:50:55 +0100

    Add sprint mechanic and camera zoom for player
    Implemented sprinting for the player, consuming hunger when sprinting and adjusting speed. Added camera zoom changes based on sprint state. Updated input map to include 'shift_sprint' action. Inventory UI is now hidden by default. Minor adjustments to main menu layout.

--- game/Scripts/Player.gd
+++ game/Scripts/Player.gd
@@ -1,12 +1,20 @@
 extends CharacterBody2D
 
 @export var speed: int = 300
+@export var sprint_speed: int = 400
 @export var health: int = 500
 @export var hunger: int = 300
 @export var can_move: bool = true
 @onready var hunger_bar: Node = get_node("../UI_Player/HungerBar")
 @onready var health_bar: Node = get_node("../UI_Player/HealthBar")
 
+@onready var cam: Camera2D = $Camera2D
+
+@export var zoom_normal := Vector2(1.8, 1.8)
+@export var zoom_sprint := Vector2(1.5, 1.5)   # im mniejsze, tym bliżej
+@export var zoom_speed := 8.0                    # szybkość 
+
+var sprinting = false
 
 
 @export var inventory: Inventory
@@ -17,7 +25,21 @@ func get_handle_input():
 		return
 		
 	var input_direction = Input.get_vector("move_left", "move_right","move_up","move_down")
-	velocity = input_direction *speed
+	
+	var current_speed = speed
+	
+	sprinting = false
+	
+	if Input.is_action_pressed("shift_sprint"):
+		if hunger >0:
+			sprinting = true;
+			current_speed = sprint_speed
+		else:
+			print("jestes na to zbyt glodny ")
+		
+	
+	
+	velocity = input_direction *current_speed
 func _physics_process(_delta):
 	if can_move:
 		get_handle_input()
@@ -26,16 +48,20 @@ func _physics_process(_delta):
 		velocity = Vector2.ZERO
 	#z_index = int(global_position.y)
 	
+	var target_zoom = zoom_sprint if sprinting else zoom_normal
+	cam.zoom = cam.zoom.lerp(target_zoom, zoom_speed * _delta)
+	
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("pause_menu") and scene_file_path!="res://menu/main_manu.tscn":
 		get_tree().change_scene_to_file("res://menu/main_menu.tscn")
+		
+	
 
 func _on_hunger_timer_timeout():
-	if hunger >0:
-		hunger -= 1
-	else:
-		if health > 0:
-			health-=1
+	if sprinting:
+		if hunger >0: 
+			hunger -= 10 
+	
 	print("Hunger:", hunger)
 	print("Health:", health)
 	
@@ -48,11 +74,18 @@ func _on_hunger_timer_timeout():
 func _ready():
 	print("Grupy gracza:", get_groups())
 	
-
-
 	
 func collect(item):
-		inventory.insert(item)
+	var cost = 50
+
+	if hunger >= cost:
+		hunger -= cost
+	else:
+		var missing = cost - hunger  
+		hunger = 0
+		health -= missing 
+	
+	inventory.insert(item)
 # Called by UI or other systems to indicate the inventory UI is open/closed.
 # When inventory is open we prevent movement; when closed we restore movement.
 func set_inventory_opened(opened: bool) -> void:
